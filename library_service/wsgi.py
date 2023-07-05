86c8e20c568373a4058facbe7c5b33d3fd82367b
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-07-05 18:37:20 +0300

    Refactored library-service to library_service

--- library_service/wsgi.py
+++ library_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for library-service project.
+WSGI config for library_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library-service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_service.settings')
 
 application = get_wsgi_application()
