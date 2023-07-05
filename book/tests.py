fed0909084493185f3c76eb39e379fe33ec4578b
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-07-05 11:06:48 +0300

    Implement tests for book

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,62 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from book.models import Book
+
+BOOK_URL = reverse("book:book-list")
+
+
+def sample_book(**params):
+    defaults = {
+        "title": "Sample movie",
+        "author": "Sample description",
+        "cover": "SOFT",
+        "inventory": 1,
+        "daily_fee": 2,
+    }
+    defaults.update(params)
+
+    return Book.objects.create(**defaults)
+
+
+def detail_url(book_id):
+    return reverse("book:book-detail", args=[book_id])
+
+
+class GetCreateBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(BOOK_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_book(self):
+        payload = {
+            "title": "Sample movie",
+            "author": "Sample description",
+            "cover": "SOFT",
+            "inventory": 1,
+            "daily_fee": 1,
+        }
+        res = self.client.post(BOOK_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+        res = self.client.post(BOOK_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.user = get_user_model().objects.create_user(
+            "admin@admin.com", "testpass", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+        res = self.client.post(BOOK_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
