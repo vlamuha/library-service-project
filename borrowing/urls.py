06e9d54a249fa9b83675d4fcaba6134a4c67f376
Vladyslav Khokhlov <63713268+vlamuha@users.noreply.github.com>
2023-07-06 15:38:53 +0300

    Update urls.py

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from borrowing.views import (
     BorrowingViewSet,
     PaymentViewSet,
-    create_checkout_session,
+    CreateCheckoutSessionView
 )
 
 router = DefaultRouter()
@@ -13,7 +13,7 @@ router.register(r'payments', PaymentViewSet, basename='payments')
 urlpatterns = [
     path(
         'payments/create-checkout-session/<int:borrowing_id>/',
-        create_checkout_session,
+        CreateCheckoutSessionView,
         name='create-checkout-session'
     ),
 ] + router.urls
