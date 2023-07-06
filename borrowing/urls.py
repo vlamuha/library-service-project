ce23399bd3df4eccf6ca228b4ea22b22982ab208
Vladyslav Khokhlov <63713268+vlamuha@users.noreply.github.com>
2023-07-06 15:34:15 +0300

    Update urls.py

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,52 +1,21 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from borrowing.views import (
     BorrowingViewSet,
     PaymentViewSet,
     create_checkout_session,
 )
 
+router = DefaultRouter()
+router.register(r'borrowings', BorrowingViewSet, basename='borrowings')
+router.register(r'payments', PaymentViewSet, basename='payments')
 
 urlpatterns = [
     path(
-        "borrowings/",
-        BorrowingViewSet.as_view({"get": "list", "post": "create"}),
-        name="borrowings-list",
-    ),
-    path(
-        "borrowings/<int:pk>/",
-        BorrowingViewSet.as_view({"get": "retrieve"}),
-        name="borrowings-detail",
-    ),
-    path(
-        "borrowings/<int:pk>/return/",
-        BorrowingViewSet.as_view({"post": "return_book"}),
-        name="borrowings-return",
-    ),
-    path(
-        "payments/",
-        PaymentViewSet.as_view({"get": "list"}),
-        name="payments-list",
-    ),
-    path(
-        "payments/<int:pk>/",
-        PaymentViewSet.as_view({"get": "retrieve"}),
-        name="payments-detail",
-    ),
-    path(
-        "payments/create-checkout-session/<int:borrowing_id>/",
+        'payments/create-checkout-session/<int:borrowing_id>/',
         create_checkout_session,
-        name="create-checkout-session",
+        name='create-checkout-session'
     ),
-    path(
-        "payments/<int:pk>/success/",
-        PaymentViewSet.as_view({"get": "payment_success"}),
-        name="payment-success",
-    ),
-    path(
-        "payments/<int:pk>/cancel/",
-        PaymentViewSet.as_view({"get": "cancel_payment"}),
-        name="cancel-payment",
-    ),
-]
+] + router.urls
 
-app_name = "borrowing"
+app_name = 'borrowing'
