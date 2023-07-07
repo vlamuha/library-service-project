0240b681e29d93c44288b02c9b60771cb7f63d35
Vladyslav Khokhlov <63713268+vlamuha@users.noreply.github.com>
2023-07-07 10:02:39 +0300

    Update models.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,51 +1,49 @@
-from user.models import User
 from django.db import models
 
-from book.models import Book
+from books.models import Book
+from users.models import User
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField(default=None, null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowings")
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="borrowings"
+    )
+
+    def __str__(self):
+        return f"{self.user.email}: {self.book.title}"
 
     @property
-    def total_fine_amount(self):
-        if self.actual_return_date:
-            days_late = (self.actual_return_date - self.expected_return_date).days
-            if days_late > 0:
-                return (self.book.daily_fee * days_late) * 2
-        days_late = (self.expected_return_date - self.borrow_date).days
-        if days_late > 0:
-            return self.book.daily_fee * days_late
-        return self.book.daily_fee * 1
+    def total_price(self):
+        return (
+            self.book.dayle_fee
+            * (self.expected_return_date - self.borrow_date).days
+        )
 
     @property
-    def fine_days(self):
-        if self.actual_return_date is not None:
-            return (self.actual_return_date - self.expected_return_date).days
-        return False
+    def fine_price(self):
+        return (
+            self.book.dayle_fee
+            * (self.actual_return_date - self.expected_return_date).days
+        ) * 2
 
 
 class Payment(models.Model):
-    STATUS_CHOICES = (
-        ("PENDING", "PENDING"),
-        ("PAID", "PAID"),
-    )
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        PAID = "Paid"
 
-    TYPE_CHOICES = (
-        ("PAYMENT", "PAYMENT"),
-        ("FINE", "FINE"),
-    )
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "Payment"
+        FINE = "Fine"
 
-    status = models.CharField(
-        max_length=32, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0]
-    )
-    type_session = models.CharField(
-        max_length=24, choices=TYPE_CHOICES, default=TYPE_CHOICES[0][0]
-    )
+    status = models.CharField(max_length=50, choices=StatusChoices.choices)
+    type = models.CharField(max_length=50, choices=TypeChoices.choices)
     borrowing = models.ForeignKey(
         Borrowing, on_delete=models.CASCADE, related_name="payments"
     )
@@ -53,9 +51,5 @@ class Payment(models.Model):
     session_id = models.CharField(max_length=255, null=True, blank=True)
     money_to_pay = models.DecimalField(max_digits=8, decimal_places=2)
 
-    def __str__(self) -> str:
-        return (
-            f"{self.get_status_display()}, "
-            f"{self.get_type_session_display()} - "
-            f"{self.borrowing}"
-        )
+    def __str__(self):
+        return f"Payment #{self.id}"
